bcbfc54b7635ebd8fbb5235878f5143167073f93
MSVFORYOU <78690237+MSVFORYOU@users.noreply.github.com>
2021-04-21 07:30:37 +0530

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,41 @@
+var Discord = require('discord.js')
+const fs = require("fs")
+const { PREFIX } = require("../../config")
+const db = require('quick.db')
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+config: {
+    name: "help",
+    description: "Help Menu",
+    usage: "1) m/help \n2) m/help [module name]\n3) m/help [command (name or alias)]",
+    example: "1) m/help\n2) m/help utility\n3) m/help ban",
+    aliases: ['h']
+},
+run: async (bot, message, args) => {
+    let prefix;
+    if (message.author.bot || message.channel.type === "dm") return;
+        try {
+            let fetched = await db.fetch(`prefix_${message.guild.id}`);
+            if (fetched == null) {
+                prefix = PREFIX
+            } else {
+                prefix = fetched
+            }
+        } catch (e) {
+            console.log(e)
+    };
+
+if(message.content.toLowerCase() === `${prefix}help`){
+    var log = new Discord.MessageEmbed()
+    .setColor(`#dcf104`)
+    .setAuthor("Help Menu")
+    .addField(`⚒ ** ❯ WELCOME SYSTEM [1]**`,"`Welcome`")
+    .addField(`❤ ** ❯ UTILITY [3]**`,"`help`,`invite`,`stats`" )
+    .setFooter("© Msv For You")
+    .setTimestamp()
+
+message.channel.send(log);
+}
+}
+}
